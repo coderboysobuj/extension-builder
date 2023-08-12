9b5f4c7d7663be9df1c3805a665f081f20c96508
devlopersabbir <devlopersabbir@gmail.com>
2023-08-12 15:32:28 +0600

    Clean webpack configaration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 require('dotenv/config');
 
@@ -55,8 +54,6 @@ const config = {
       DEV: JSON.stringify(!isProduction),
       VERSION: JSON.stringify(version)
     })
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
 
 
@@ -75,9 +72,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
